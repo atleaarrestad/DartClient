392714f51bb5ce8fd7f55ad4495a538131269599
DESKTOP-N8B13P6\Atle <atleaarrestad@gmail.com>
2025-11-06 19:52:37 +0100

    rank renaming

--- src/models/rank.ts
+++ src/models/rank.ts
@@ -1,50 +1,50 @@
 import { getAbsoluteBase } from '../getAbsoluteBase.js';
 const base = getAbsoluteBase();
 export enum Rank {
-	Bronze4 = 0,
-	Bronze3 = 1,
+	Bronze = 0,
+	Bronze1 = 1,
 	Bronze2 = 2,
-	Bronze1 = 3,
-	Silver4 = 4,
-	Silver3 = 5,
+	Bronze3 = 3,
+	Silver = 4,
+	Silver1 = 5,
 	Silver2 = 6,
-	Silver1 = 7,
-	Gold4 = 8,
-	Gold3 = 9,
+	Silver3 = 7,
+	Gold = 8,
+	Gold1 = 9,
 	Gold2 = 10,
-	Gold1 = 11,
-	Platinum4 = 12,
-	Platinum3 = 13,
+	Gold3 = 11,
+	Platinum = 12,
+	Platinum1 = 13,
 	Platinum2 = 14,
-	Platinum1 = 15,
-	Diamond4 = 16,
-	Diamond3 = 17,
+	Platinum3 = 15,
+	Diamond = 16,
+	Diamond1 = 17,
 	Diamond2 = 18,
-	Diamond1 = 19,
+	Diamond3 = 19,
 	Grandmaster = 20,
 }
 
 export const RankDisplayValues: Record<Rank, string> = {
-	[Rank.Bronze4]: "Bronze IV",
-	[Rank.Bronze3]: "Bronze III",
-	[Rank.Bronze2]: "Bronze II",
+	[Rank.Bronze]: "Bronze",
 	[Rank.Bronze1]: "Bronze I",
-	[Rank.Silver4]: "Silver IV",
-	[Rank.Silver3]: "Silver III",
-	[Rank.Silver2]: "Silver II",
+	[Rank.Bronze2]: "Bronze II",
+	[Rank.Bronze3]: "Bronze III",
+	[Rank.Silver]: "Silver",
 	[Rank.Silver1]: "Silver I",
-	[Rank.Gold4]: "Gold IV",
-	[Rank.Gold3]: "Gold III",
-	[Rank.Gold2]: "Gold II",
+	[Rank.Silver2]: "Silver II",
+	[Rank.Silver3]: "Silver III",
+	[Rank.Gold]: "Gold",
 	[Rank.Gold1]: "Gold I",
-	[Rank.Platinum4]: "Platinum IV",
-	[Rank.Platinum3]: "Platinum III",
-	[Rank.Platinum2]: "Platinum II",
+	[Rank.Gold2]: "Gold II",
+	[Rank.Gold3]: "Gold III",
+	[Rank.Platinum]: "Platinum",
 	[Rank.Platinum1]: "Platinum I",
-	[Rank.Diamond4]: "Diamond IV",
-	[Rank.Diamond3]: "Diamond III",
-	[Rank.Diamond2]: "Diamond II",
+	[Rank.Platinum2]: "Platinum II",
+	[Rank.Platinum3]: "Platinum III",
+	[Rank.Diamond]: "Diamond",
 	[Rank.Diamond1]: "Diamond I",
+	[Rank.Diamond2]: "Diamond II",
+	[Rank.Diamond3]: "Diamond III",
 	[Rank.Grandmaster]: "Grandmaster",
 };
 
@@ -64,8 +64,8 @@ export function getRankIcon(rank: Rank | undefined): string {
 			return `${base}icons/bronze_2.png`;
 		case Rank.Bronze3:
 			return `${base}icons/bronze_3.png`;
-		case Rank.Bronze4:
-			return `${base}icons/bronze_4.png`;
+		case Rank.Bronze:
+			return `${base}icons/bronze.png`;
 
 		case Rank.Silver1:
 			return `${base}icons/silver_1.png`;
@@ -73,8 +73,8 @@ export function getRankIcon(rank: Rank | undefined): string {
 			return `${base}icons/silver_2.png`;
 			case Rank.Silver3:
 			return `${base}icons/silver_3.png`;
-		case Rank.Silver4:
-			return `${base}icons/silver_4.png`;
+		case Rank.Silver:
+			return `${base}icons/silver.png`;
 
 		case Rank.Gold1:
 			return `${base}icons/gold_1.png`;
@@ -82,8 +82,8 @@ export function getRankIcon(rank: Rank | undefined): string {
 			return `${base}icons/gold_2.png`;
 		case Rank.Gold3:
 			return `${base}icons/gold_3.png`;
-		case Rank.Gold4:
-			return `${base}icons/gold_4.png`;
+		case Rank.Gold:
+			return `${base}icons/gold.png`;
 
 		case Rank.Platinum1:
 			return `${base}icons/platinum_1.png`;
@@ -91,8 +91,8 @@ export function getRankIcon(rank: Rank | undefined): string {
 			return `${base}icons/platinum_2.png`;
 		case Rank.Platinum3:
 			return `${base}icons/platinum_3.png`;
-		case Rank.Platinum4:
-			return `${base}icons/platinum_4.png`;
+		case Rank.Platinum:
+			return `${base}icons/platinum.png`;
 
 		case Rank.Diamond1:
 			return `${base}icons/diamond_1.png`;
@@ -100,8 +100,8 @@ export function getRankIcon(rank: Rank | undefined): string {
 			return `${base}icons/diamond_2.png`;
 		case Rank.Diamond3:
 			return `${base}icons/diamond_3.png`;
-		case Rank.Diamond4:
-			return `${base}icons/diamond_4.png`;
+		case Rank.Diamond:
+			return `${base}icons/diamond.png`;
 
 		case Rank.Grandmaster:
 			return `${base}icons/grandmaster.png`;
